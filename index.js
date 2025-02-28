60f3a1acb0a3e4d86a0c01666e2aaed7baf6730f
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-02-28 15:36:12 +0600

    how to add array list

---
+++ index.js
@@ -0,0 +1,15 @@
+const numbers = [44, 75, 74, 8, 94, 364];
+
+// console.log(numbers);
+// console.log(numbers[0]);
+
+// get element value by index
+const fourth = numbers[3];
+console.log(fourth);
+
+// set or update element value by index
+let abc = 45;
+abc = 99;
+numbers[1] = abc;
+
+console.log(numbers);
